217a055c5b7c7f1c6bb46358aa1d53951e5e310a
Nevarez Marketing <35445349+theoccultcorner@users.noreply.github.com>
2025-06-04 12:27:10 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,7 +67,7 @@ const Home = () => {
            <Typography variant="body1" gutterBottom>
           To the "Sharing the Message" group of Narcotics Anonymous Community Homepage.
         </Typography>
-           <Typography variant="h4" gutterBottom>
+           <Typography variant="h2" gutterBottom>
           NA Service Prayer
         </Typography>
                    <Typography variant="body1" gutterBottom>
