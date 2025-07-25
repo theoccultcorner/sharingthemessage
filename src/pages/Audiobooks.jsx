013ec167c8524e16d5ced0e652f36745b5101c3d
Raven The wolf <theoccultcorner@gmail.com>
2025-07-25 12:43:30 -0700

    F

--- src/pages/Audiobooks.jsx
+++ src/pages/Audiobooks.jsx
@@ -3,10 +3,21 @@ import React from "react";
 
 const Audiobooks = () => {
   return (
-    <div style={{ padding: "1rem" }}>
+    <div style={{ padding: "1rem", maxWidth: "800px", margin: "0 auto" }}>
       <h1>Audiobooks</h1>
-      <p>Listen to recovery-focused or inspirational audiobooks here.</p>
-      {/* Later you can add an audio player, playlist, etc. */}
+      <p>Listen to the full Basic Text of Narcotics Anonymous below.</p>
+
+      {/* Embedded YouTube playlist for NA Basic Text */}
+      <div style={{ position: "relative", paddingBottom: "56.25%", height: 0, overflow: "hidden", marginTop: "1rem" }}>
+        <iframe
+          src="https://www.youtube.com/embed/videoseries?list=PLHeh4PPjtVu2CQXLo9yB34V8WJCDYdoRR"
+          title="Narcotics Anonymous Basic Text Playlist"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+        ></iframe>
+      </div>
     </div>
   );
 };
